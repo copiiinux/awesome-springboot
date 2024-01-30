269b2d1dd0eb48e50d1b0bed90de5d6e2e9f51de
Loïck Roth <loick.roth@lahoco.ch>
2024-01-30 20:14:04 +0100

    Added Spring-Boot project base files

---
+++ src/main/java/ch/copiiinux/awesomespringboot/AwesomeSpringbootApplication.java
@@ -0,0 +1,14 @@
+package ch.copiiinux.awesomespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomeSpringbootApplication.class,
+                              args);
+    }
+
+}
---
+++ src/test/java/ch/copiiinux/awesomespringboot/AwesomeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.copiiinux.awesomespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
